231e89d60b78704a5ff2a59520145bbba90c5dfe
Nelson <nelson_buarque@hotmail.com>
2022-01-31 15:49:24 -0300

    Update main.tf

--- examples/fmc_network_group_objects/main.tf
+++ examples/fmc_network_group_objects/main.tf
@@ -14,41 +14,59 @@ provider "fmc" {
   fmc_insecure_skip_verify = var.fmc_insecure_skip_verify
 }
 
-data "fmc_network_objects" "PrivateVLAN" {
-  name = "VLAN825-Private"
+#data "fmc_network_objects" "PrivateVLAN" {
+#  name = "VLAN825-Private"
+#}
+
+#resource "fmc_network_objects" "PrivateVLANDR" {
+#  name        = "DRsite-VLAN"
+#  value       = data.fmc_network_objects.PrivateVLAN.value
+#  description = "testing terraform"
+#
+
+
+resource "fmc_network_objects" "terraform_test_host_6" {
+  name        = "terraform_test_host_6"
+  value       = "192.168.44.0/24"
+  description = "testing terraform change"
 }
 
-resource "fmc_network_objects" "PrivateVLANDR" {
-  name        = "DRsite-VLAN"
-  value       = data.fmc_network_objects.PrivateVLAN.value
-  description = "testing terraform"
+resource "fmc_network_objects" "terraform_test_host_5" {
+  name        = "terraform_test_host_5"
+  value       = "1.1.1.200/32"
+  description = "testing terraform change"
 }
 
-resource "fmc_network_group_objects" "TestPrivateGroup" {
-  name = "TestPrivateGroup"
+
+resource "fmc_network_group_objects" "terraform_TestPrivateGroup" {
+  name = "terraform_TestPrivateGroup"
   description = "Testing groups"
   objects {
-      id = data.fmc_network_objects.PrivateVLAN.id
-      type = data.fmc_network_objects.PrivateVLAN.type
+      id = fmc_network_objects.terraform_test_host_6.id
+      type = fmc_network_objects.terraform_test_host_6.type
   }
   objects {
-      id = fmc_network_objects.PrivateVLANDR.id
-      type = fmc_network_objects.PrivateVLANDR.type
+      id = fmc_network_objects.terraform_test_host_5.id
+      type = fmc_network_objects.terraform_test_host_5.type
   }
   literals {
-      value = "10.10.10.10"
+      value = "100.100.100.222"
       type = "Host"
   }
 }
 
-output "existing_fmc_network_object" {
-  value = data.fmc_network_objects.PrivateVLAN
-}
+#output "existing_fmc_network_object" {
+#  value = data.fmc_network_objects.PrivateVLAN
+#}
 
 output "new_fmc_network_object" {
-  value = fmc_network_objects.PrivateVLANDR
+  value = fmc_network_objects.terraform_test_host_6
+}
+
+output "new_fmc_network_object_2" {
+  value = fmc_network_objects.terraform_test_host_5
 }
 
 output "new_fmc_network_group_object" {
-  value = fmc_network_group_objects.TestPrivateGroup
+  value = fmc_network_group_objects.terraform_TestPrivateGroup
 }
