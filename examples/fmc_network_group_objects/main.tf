41d3085a1c21d57199b7856d0fcf9bd64f70205b
Nelson <nelson_buarque@hotmail.com>
2022-02-03 09:36:27 -0300

    Update main.tf

--- examples/fmc_network_group_objects/main.tf
+++ examples/fmc_network_group_objects/main.tf
@@ -25,29 +25,29 @@ provider "fmc" {
 #
 
 
-resource "fmc_network_objects" "terraform_test_host_6" {
-  name        = "terraform_test_host_6"
+resource "fmc_network_objects" "terraform_test_host_7" {
+  name        = "terraform_test_host_7"
   value       = "192.168.44.0/24"
   description = "testing terraform change"
 }
 
-resource "fmc_network_objects" "terraform_test_host_5" {
-  name        = "terraform_test_host_5"
+resource "fmc_network_objects" "terraform_test_host_8" {
+  name        = "terraform_test_host_8"
   value       = "10.0.1.0/24"
   description = "testing terraform change"
 }
 
 
-resource "fmc_network_group_objects" "terraform_TestPrivateGroup" {
-  name = "terraform_TestPrivateGroup"
+resource "fmc_network_group_objects" "terraform_TestPrivateGroup_1" {
+  name = "terraform_TestPrivateGroup_1"
   description = "Testing groups"
   objects {
-      id = fmc_network_objects.terraform_test_host_6.id
-      type = fmc_network_objects.terraform_test_host_6.type
+      id = fmc_network_objects.terraform_test_host_7.id
+      type = fmc_network_objects.terraform_test_host_7.type
   }
   objects {
-      id = fmc_network_objects.terraform_test_host_5.id
-      type = fmc_network_objects.terraform_test_host_5.type
+      id = fmc_network_objects.terraform_test_host_8.id
+      type = fmc_network_objects.terraform_test_host_8.type
   }
   literals {
       value = "100.100.100.222"
@@ -60,13 +60,13 @@ resource "fmc_network_group_objects" "terraform_TestPrivateGroup" {
 #}
 
 output "new_fmc_network_object" {
-  value = fmc_network_objects.terraform_test_host_6
+  value = fmc_network_objects.terraform_test_host_7
 }
 
 output "new_fmc_network_object_2" {
-  value = fmc_network_objects.terraform_test_host_5
+  value = fmc_network_objects.terraform_test_host_8
 }
 
-#output "new_fmc_network_group_object" {
-#  value = fmc_network_group_objects.terraform_TestPrivateGroup
-#}
+output "new_fmc_network_group_object" {
+  value = fmc_network_group_objects.terraform_TestPrivateGroup_1
+}
